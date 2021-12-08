21ec4b714a17091035f458aa765c387171fc340d
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2021-12-08 12:26:20 -0600

    html and js changes, added css file

---
+++ script.js
@@ -0,0 +1,8 @@
+function initPage() {
+    
+    const APIKey = "b31128cf2f45a7d81447683eba5469c1";
+        function getWeather(cityName) {
+            let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        }
+}
+initPage();
