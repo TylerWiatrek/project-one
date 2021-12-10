4de29a58b6f0308b88becc8dcf9d658ad8cd993b
Tyler Wiatrek <wiatrektyler@yahoo.com>
2021-12-09 20:26:32 -0600

    added functionality

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-function initPage() {
+var searchCityEl = document.querySelector("#sunrise-form");
+var cityInputEl = document.querySelector("#city");
+
+var getCityCoordinates = function(city) {
     
-    const APIKey = "b31128cf2f45a7d81447683eba5469c1";
-        function getWeather(cityName) {
-            let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-        }
+    var apiCoordURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2ea8fea258bbf4c7d30e6cc6067e3356`
+    fetch(apiCoordURL)
+    .then(function(response) {
+        return response.json()
+    }).then(function(data){
+        console.log(data.coord)
+        getSunriseSunset(data.coord)
+       
+        });
+};
+
+
+
+
+var getSunriseSunset = function(coordinates) {
+    var {lat} = coordinates;
+    var {lon} = coordinates;
+    console.log(lat);
+    console.log(lon);
+    var apiSunrise = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}`
+    fetch(apiSunrise)
+    .then(function(response) {
+        return response.json()
+    }).then(function(data) {
+        console.log(data)
+    });
+};
+
+
+var getSearch = function(event) {
+    event.preventDefault();
+    var cityName = cityInputEl.value.trim();
+    getCityCoordinates(cityName);
 }
-initPage();
+
+searchCityEl.addEventListener("submit", getSearch);
